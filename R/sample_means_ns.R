bb763464b6917a30416bb9680647e804b190f531
Aiden Nguyen <63250552+anguy509@users.noreply.github.com>
2021-05-24 00:21:13 -0700

    create sample_means_ns.R

---
+++ R/sample_means_ns.R
@@ -0,0 +1,22 @@
+#' Perform the many_sample_means process for each of the values in the ns vector and return a data frame with the results
+#'
+#' @param vec A vector that will be sampled from
+#' @param reps An integer reps that represents how many times many_sample_means will be performed
+#' @param ns A vector representing the different values of n that will be passed into many_sample means
+#'
+#' @return A data frame with the results
+#'
+#' @import stringr
+#' @import dplyr
+#' @import glue
+#' @import purrr
+#'
+#' @export
+sample_means_ns <- function(vec, reps, ns)
+{
+  x <- map(ns, ~many_sample_means(vec, .x, reps))
+  x <- unlist(x)
+  y <- rep(ns, reps)
+  
+  return(tibble(sample_mean = x, n = y))
+}
